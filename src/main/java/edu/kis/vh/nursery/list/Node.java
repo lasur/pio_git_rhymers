73fea5484fdfe2ea43b5f743361f320bd7dcba84
Jarosław Pietrzak <jarpietrzak@gmail.com>
2019-12-03 14:39:33 +0100

    Hermetyzacja

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,12 +1,35 @@
 package edu.kis.vh.nursery.list;
 
-public class Node {
+class Node {
 
-    public int value;
-    public Node prev, next;
+    private int value;
+    private Node prev, next;
 
-    public Node(int i) {
-        value = i;
+    Node(int i) {
+        setValue(i);
     }
 
+    int getValue() {
+        return value;
+    }
+
+    private void setValue(int value) {
+        this.value = value;
+    }
+
+    Node getPrev() {
+        return prev;
+    }
+
+    void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    Node getNext() {
+        return next;
+    }
+
+    void setNext(Node next) {
+        this.next = next;
+    }
 }
