7a1250d76affb2403cfb77b99cdb76aedaa91bb4
Jarosław Pietrzak <jarpietrzak@gmail.com>
2019-12-03 13:50:25 +0100

    Format

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,18 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
 }
+//skrót alt + strzałka w lewo przechodzi do następnego słowa w Eclipse w Intellij ten skrót to ctrl + strzałka w lewo
+//skrót alt + strzałka w prawo przechodzi do poprzedniego słowa w Eclipse w Intellij ten skrót to ctrl + strzałka w prawo
